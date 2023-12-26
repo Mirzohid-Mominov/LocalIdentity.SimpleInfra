6d26b76a27b89c7aaad52010c5671f8d36468221
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-12-26 22:40:07 +0500

    created Brokers and DataContexts for Persistence

---
+++ LocalIdentity.SimpleInfra.Persistence/Caching/Brokers/ICacheBroker.cs
@@ -0,0 +1,16 @@
+﻿using LocalIdentity.SimpleInfra.Domain.Common.Caching;
+
+namespace LocalIdentity.SimpleInfra.Persistence.Caching.Brokers;
+
+public interface ICacheBroker
+{
+   ValueTask<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default);
+   
+   ValueTask<bool> TryGetAsync<T>(string key, out T? value, CancellationToken cancellationToken = default);
+
+   ValueTask<T?> GetOrSetAsync<T>(string key, Func<Task<T>> valueFactory, CacheEntyOptions? entryOptions = default, CancellationToken cancellationToken = default);
+   
+   ValueTask SetAsync<T>(string key, T value, CacheEntyOptions? entryOptions = default, CancellationToken cancellationToken = default);
+
+   ValueTask DeleteAsync<T>(string key, CancellationToken cancellationToken = default);
+}
